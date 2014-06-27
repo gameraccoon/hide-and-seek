692ba653f6701cf2e589c0da31eee3ce6cfffa76
Pavel Grebnev <mail@ruwhynot.com>
2014-06-27 18:17:40 +0300

    Removed some unused code from the Editor project. Added light emitter that follows the mouse cursor.

--- src/Editor/main.cpp
+++ src/Editor/main.cpp
@@ -2,7 +2,6 @@
 
 #include <HgeInterface/Input/KeyListeners.h>
 #include <HgeInterface/Graphics/FloatingCamera.h>
-#include <HgeInterface/Graphics/DirectionArrow.h>
 
 #include <Engine/Core/World.h>
 #include <Engine/Structures/PathPoint.h>
@@ -23,86 +22,28 @@ HGE *hge = nullptr;
 hgeSprite*	crosshair;
 hgeFont*	font;
 
-//#define FULLSCREEN
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const bool FULL_SCREEN = false;
 
-#ifndef FULLSCREEN
-	const int SCREEN_WIDTH = 800;
-	const int SCREEN_HEIGHT = 600;
-	const bool FULL_SCREEN = false;
-#else
-	const int SCREEN_WIDTH = 1366;
-	const int SCREEN_HEIGHT = 768;
-	const bool FULL_SCREEN = true;
-#endif
-
-const Vector2D SCREEN_CENTER(SCREEN_WIDTH/2, SCREEN_HEIGHT/2);
-
-const float MAX_CAMERA_RANGE = (float)std::min(SCREEN_HEIGHT, SCREEN_WIDTH) / 4.0f;
-
-const float HERO_SPEED = 100.f;
-
-Vector2D mousePos = ZERO_VECTOR;
+Vector2D mousePos(ZERO_VECTOR);
 
 float cameraAngle = 0.0f;
 
-// The Hero whom we control
-Hero *ourHero;
-
-// Our big World =)
+// World that we edit
 World *gameWorld;
 
 // Camera
-FloatingCamera *mainCamera;
+Camera *mainCamera;
 
-// Test arrow for show directions on screen
-DirectionArrow *arrow;
+Vector2D cameraWorldLocation(ZERO_VECTOR);
 
 // Button
 ButtonListeners listeners;
 
-// event listeners
-class BtnShadows : public ButtonSwitcher
-{
-public:
-	BtnShadows(HGE *hge) : ButtonSwitcher(hge, HGEK_H, true) { };
-	void pressed() { ::mainCamera->showShadows(bActive); }
-};
+LightEmitter *cameraLight;
 
-class BtnFog : public ButtonSwitcher
-{
-public:
-	BtnFog(HGE *hge) : ButtonSwitcher(hge, HGEK_F, true) { };
-	void pressed() { ::mainCamera->showFog(bActive); }
-};
-
-class BtnLights : public ButtonSwitcher
-{
-public:
-	BtnLights(HGE *hge) : ButtonSwitcher(hge, HGEK_L, false) { };
-	void pressed() { ::mainCamera->showLights(bActive); }
-};
-
-class BtnAABB : public ButtonSwitcher
-{
-public:
-	BtnAABB(HGE *hge) : ButtonSwitcher(hge, HGEK_C, false) { };
-	void pressed() { ::mainCamera->showAABB(bActive); }
-};
-
-class BtnHulls : public ButtonSwitcher
-{
-public:
-	BtnHulls(HGE *hge) : ButtonSwitcher(hge, HGEK_M, false) { };
-	void pressed() { ::mainCamera->showHulls(bActive); }
-};
-
-class BtnPaths : public ButtonSwitcher
-{
-public:
-	BtnPaths(HGE *hge) : ButtonSwitcher(hge, HGEK_P, false) { };
-	void pressed() { ::mainCamera->showPaths(bActive); }
-};
-
+/*
 class BtnShoot : public ButtonSwitcher
 {
 public:
@@ -116,7 +57,7 @@ class BtnAddLight : public ButtonSwitcher
 public:
 	BtnAddLight(HGE *hge) : ButtonSwitcher(hge, HGEK_RBUTTON, true) { };
 	void pressed() { new LightEmitter(::gameWorld, ::mainCamera->deProject(::mousePos), Vector2D(1.f, 1.f), Rotator(0.f)); }
-};
+};*/
 
 bool FrameFunc()
 {
@@ -133,29 +74,18 @@ bool FrameFunc()
 
 	if (::hge->Input_GetKeyState(HGEK_Q))	::cameraAngle += 0.005f;
 	if (::hge->Input_GetKeyState(HGEK_E))	::cameraAngle -= 0.005f;
-	
-	if (Direction.size() > 0)
-	{
-		::ourHero->move(Vector2D(Direction.rotation() - ::cameraAngle) * ::HERO_SPEED);
-	}
-	::ourHero->setRotation((::mainCamera->deProject(::mousePos) - ::ourHero->getLocation()).rotation());
+
+	cameraWorldLocation += Direction;
 
 	::hge->Input_GetMousePos(&::mousePos.x, &::mousePos.y);
 
-	Vector2D CameraShift((::mousePos - SCREEN_CENTER)/2);
-	CameraShift = CameraShift.ort() * std::min(CameraShift.size(), MAX_CAMERA_RANGE);
-	::mainCamera->setLocation(::ourHero->getLocation());
+	cameraLight->setLocation(mainCamera->deProject(mousePos));
+
+	::mainCamera->setLocation(cameraWorldLocation);
 	::mainCamera->setRotation(::cameraAngle);
-	::mainCamera->setCenterShift(CameraShift);
 
 	// Update key states
 	::listeners.check();
-
-	// Do World update
-	::gameWorld->update(dt);
-
-	::arrow->setVDirection(Direction);
-	::arrow->setCenter(SCREEN_CENTER - CameraShift);
 
 	return false;
 }
@@ -175,25 +105,9 @@ bool RenderFunc()
 	cameraRenderSprite->Render(0, 0);
 
 	::crosshair->Render(::mousePos.x, ::mousePos.y);
-	::arrow->render();
 
 	// fps and dt
 	::font->printf(5, 5, HGETEXT_LEFT, "dt:%.3f\nFPS:%d", ::hge->Timer_GetDelta(), ::hge->Timer_GetFPS());
-	
-	// Status of rendering elements
-	if (::listeners.isActive(HGEK_M))
-		::font->printf(5, 60, HGETEXT_LEFT, "Showing Models");
-	if (::listeners.isActive(HGEK_C))
-		::font->printf(5, 90, HGETEXT_LEFT, "Showing Bounding boxes");
-	if (::listeners.isActive(HGEK_L))
-		::font->printf(5, 120, HGETEXT_LEFT, "Showing Light emitters");
-	if (::listeners.isActive(HGEK_P))
-		::font->printf(5, 150, HGETEXT_LEFT, "Showing Paths");
-
-	if (!::listeners.isActive(HGEK_F))
-		::font->printf(5, 180, HGETEXT_LEFT, "Hidded Fog");
-	if (!::listeners.isActive(HGEK_H))
-		::font->printf(5, 210, HGETEXT_LEFT, "Hidded Shadows");
 
 	//-- Stop render graphics
 
@@ -211,7 +125,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	::hge->System_SetState(HGE_LOGFILE, "SG.log");
 	::hge->System_SetState(HGE_FRAMEFUNC, FrameFunc);
 	::hge->System_SetState(HGE_RENDERFUNC, RenderFunc);
-	::hge->System_SetState(HGE_TITLE, "Stealth game - alpha1");
+	::hge->System_SetState(HGE_TITLE, "Stealth game - Editor");
 	::hge->System_SetState(HGE_FPS, 100);
 	::hge->System_SetState(HGE_WINDOWED, !FULL_SCREEN);
 	::hge->System_SetState(HGE_SCREENWIDTH, SCREEN_WIDTH);
@@ -240,27 +154,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 			::gameWorld = new World();
 			WorldsContainer::Container().insertWorld(::gameWorld, "mainWorld");
 
-			::mainCamera = new FloatingCamera(::hge, ::gameWorld, SCREEN_CENTER * 2, Vector2D(0.0f, 0.0f));
+			::mainCamera = new Camera(::hge, ::gameWorld, Vector2D((float)SCREEN_WIDTH, (float)SCREEN_HEIGHT), Vector2D(0.0f, 0.0f));
+			::mainCamera->showFog(false);
 
-			// hero will be deleted automaticaly as other actors
-			::ourHero = new Hero(::gameWorld, Vector2D(0.0f, 350.0f), Vector2D(1.f, 1.f), Rotator(0.f));
+			cameraLight = new LightEmitter(::gameWorld, ZERO_VECTOR, Vector2D(0.0f, 0.0f), 0.0f);
 
-			::ourHero->giveWeapon(new Weapon());
-
-			::arrow = new DirectionArrow(::hge);
-			::arrow->setCenter(SCREEN_CENTER);
-		
-			::listeners.addListener(new BtnAABB(::hge));
-			::listeners.addListener(new BtnHulls(::hge));
-			::listeners.addListener(new BtnFog(::hge));
-			::listeners.addListener(new BtnShadows(::hge));
-			::listeners.addListener(new BtnLights(::hge));
-			::listeners.addListener(new BtnPaths(::hge));
-			::listeners.addListener(new BtnShoot(::hge));
-			::listeners.addListener(new BtnAddLight(::hge));
+//			::listeners.addListener(new BtnShoot(::hge));
+//			::listeners.addListener(new BtnAddLight(::hge));
 
 			LevelLoader::load(::gameWorld, std::string("test"));
-			//LevelLoader::save(::gameWorld, std::string("test"));
 
 			// Let's rock now!
 			::hge->System_Start();
@@ -268,7 +170,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 			// Delete created objects and free loaded resources
 			delete ::font;
 			delete ::crosshair;
-			delete ::arrow;
 			delete ::mainCamera;
 			delete ::gameWorld;
 			::hge->Texture_Free(texture);
