da0d59987fd3f7bf86179dc7ecac1086186ea361
Pavel Grebnev <mail@ruwhynot.com>
2014-07-02 00:36:46 +0300

    Added ability to move light emitters.

--- src/Engine/Actors/LightEmitter.cpp
+++ src/Engine/Actors/LightEmitter.cpp
@@ -6,6 +6,11 @@ LightEmitter::LightEmitter(World *world, Vector2D location, Vector2D scale, Rota
 	this->brightness = 1.0f;
 	this->color = 0xFFFFFFFF;
 
+	this->setOriginalSize(Vector2D(10, 10));
+	this->setScale(scale);
+
+	this->updateCollision();
+
 	this->updateActorId("LightEmitter");
 }
 
@@ -16,6 +21,7 @@ LightEmitter::~LightEmitter(void)
 
 void LightEmitter::updateCollision()
 {
+	this->setColideBox(BoundingBox(this->getLocation() - this->getCalculatedSize()/2, this->getLocation() + this->getCalculatedSize()/2));
 }
 
 float LightEmitter::getBrightness() const
