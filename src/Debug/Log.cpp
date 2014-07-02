cd5a1296759b634f910fbadd6170c55c0a38029e
Pavel Grebnev <mail@ruwhynot.com>
2014-07-02 18:34:52 +0300

    Log class changed to singleton "phoenix". This provide ability to log events in destructors of static objects. Also Log doesn't close file handler until program is closed.

--- src/Debug/Log.cpp
+++ src/Debug/Log.cpp
@@ -3,28 +3,90 @@
 #include <fstream>
 #include <ctime>
 
-void Log::WriteError(std::string text)
+Log* Log::singleInstance = nullptr;
+bool Log::isDestroyed = false;
+
+Log::Log()
 {
-	Log::WriteLine(std::string(" Error: ").append(text));
+	this->logFileStream = new std::ofstream(std::string("./logs/").append("log.txt"), std::ios_base::app);
 }
 
-void Log::WriteWarning(std::string text)
+Log::~Log()
 {
-	Log::WriteLine(std::string(" Warning: ").append(text));
+	this->logFileStream->close();
+	delete this->logFileStream;
+
+	singleInstance = nullptr;
+	isDestroyed = true;
 }
 
-void Log::WriteLog(std::string text)
+Log& Log::Instance()
 {
-	Log::WriteLine(std::string(" Log: ").append(text));
+	// if we haven't instance of the Log
+	if (Log::singleInstance == nullptr)
+	{
+		if (isDestroyed)
+		{
+			// the Log was destroyed
+			Log::onDeadReference();
+		}
+		else
+		{
+			// first access, create single instance
+			Log::singleInstance = new Log();
+		}
+	}
+
+	return *Log::singleInstance;
 }
 
-void Log::WriteLine(std::string text)
+void Log::create()
 {
-	std::ofstream logFile;
-	logFile.open(std::string("./logs/").append("log.txt"), std::ios_base::app);
+	/*
+	 * Real object that be initialized on first usage
+	 * and it be automatically destroyed on program shutdown
+	 */
+	static Log theInstance;
+	Log::singleInstance = &theInstance;
+}
 
-	logFile << std::time(0);
-	logFile << text << std::endl;
+void Log::onDeadReference()
+{
+	// Get the old singletone location in the memory (ash of the phoenix)
+	Log::create();
+	// Create new singletone on this place
+	new (singleInstance) Log;
+	// Say that we want to destroy this singletone on the application shutdown
+	atexit(killPhoenixSingletone);
+	// Say that the singletone is ready to use
+	Log::isDestroyed = false;
+}
 
-	logFile.close();
+void Log::killPhoenixSingletone()
+{
+	delete Log::singleInstance;
+}
+
+void Log::writeError(std::string text)
+{
+	this->writeLine(std::string(" Error: ").append(text));
+}
+
+void Log::writeWarning(std::string text)
+{
+	this->writeLine(std::string(" Warning: ").append(text));
+}
+
+void Log::writeLog(std::string text)
+{
+	this->writeLine(std::string(" Log: ").append(text));
+}
+
+void Log::writeLine(std::string text)
+{
+	if (this->logFileStream->is_open())
+	{
+		*this->logFileStream << std::time(0);
+		*this->logFileStream << text << std::endl;
+	}
 }
--- src/Debug/Log.h
+++ src/Debug/Log.h
@@ -3,15 +3,52 @@
 
 #include <string>
 
+/**
+ * Class that helps to write log messages.
+ * Log is a Singletone "phoenix".
+ * (see Alexandrescu "Modern C++ Design: Generic Programming and Design Patterns Applied")
+ */
 class Log
 {
 public:
-	static void WriteError(std::string text);
-	static void WriteWarning(std::string text);
-	static void WriteLog(std::string text);
+	/**
+	 * Return single instance of class Log
+	 * Usage: Log::Instance().function(...);
+	 */
+	static Log& Instance();
+
+	/* Logging functions */
+	void writeError(std::string text);
+	void writeWarning(std::string text);
+	void writeLog(std::string text);
 
 private:
-	static void WriteLine(std::string text);
+	/** Write line with timestamp */
+	void writeLine(std::string text);
+
+	/** Filestream that holds the logfile handler */
+	std::ofstream *logFileStream;
+
+	/** Construct singletone information */
+	static void create();
+
+	/** Function runs when user tries to get access to destroyed singletone */
+	static void onDeadReference();
+
+	/** function that destroy singletone with ability to construct it in future */
+	static void killPhoenixSingletone();
+
+	/** Single instance of class log */
+	static Log* singleInstance;
+
+	/** Flag shows that singletone was created and destroyed */
+	static bool isDestroyed;
+
+	/* Turn off unusable operations */
+	Log();
+	~Log();
+	Log(const Log&);
+	void operator=(const Log&);
 };
 
 #endif
