b0d0880bb5882036f569b5b6d7c9baf35eaf6eca
Pavel Grebnev <gamedevraccoon@gmail.com>
2020-07-08 20:09:12 +0300

    Fix several issues with FormatString function

--- src/Base/Types/String/StringHelpers.h
+++ src/Base/Types/String/StringHelpers.h
@@ -3,38 +3,19 @@
 #define STR_HELPER(x) #x
 #define STR(x) STR_HELPER(x)
 
-template<typename... Args>
-std::string FormatString(const std::string& format)
-{
-	return format;
-}
-
-template<typename T>
-auto FormatSprinfableArgument(T arg);
-
-template<>
-inline auto FormatSprinfableArgument<const std::string&>(const std::string& arg)
+inline const char* FormatSprinfableArgument(const std::string& arg)
 {
 	return arg.c_str();
 }
 
-template<>
-inline auto FormatSprinfableArgument<std::string&&>(std::string&& arg)
+inline const char* FormatSprinfableArgument(std::string&& arg)
 {
 	return arg.c_str();
 }
 
-template<>
-inline auto FormatSprinfableArgument<std::string>(std::string arg)
-{
-	return arg.c_str();
-}
-
-template<>
-inline auto FormatSprinfableArgument<StringID>(StringID arg)
+inline const char* FormatSprinfableArgument(StringID arg)
 {
 	static_assert(std::is_same_v<std::invoke_result_t<decltype(&StringIDManager::getStringFromID), StringIDManager, StringID>, const std::string&>, "StringIDManager::getStringFromID should return const ref to std::string");
-
 	return StringIDManager::Instance().getStringFromID(arg).c_str();
 }
 
@@ -44,10 +25,16 @@ auto FormatSprinfableArgument(T arg)
 	return std::forward<T>(arg);
 }
 
+template<typename... Args>
+std::string FormatString(const std::string& format)
+{
+	return format;
+}
+
 template<typename First, typename... Args>
 std::string FormatString(const std::string& format, First&& first, Args&&... args)
 {
 	char buf[4096];
-	snprintf(buf, sizeof(buf), format.c_str(), FormatSprinfableArgument(std::forward<First>(first)), FormatSprinfableArgument(std::forward<Args>(args))...);
+	std::snprintf(buf, sizeof(buf), format.c_str(), FormatSprinfableArgument(std::forward<First>(first)), FormatSprinfableArgument(std::forward<Args>(args))...);
 	return std::string(buf);
 }
--- src/UnitTests/Tests/TestFormatString.cpp
+++ src/UnitTests/Tests/TestFormatString.cpp
@@ -5,13 +5,13 @@
 #include "Base/Types/String/StringHelpers.h"
 
 TEST(FormatString, NumericalFormats)
-{;
+{
 	EXPECT_EQ("test10test", FormatString("test%dtest", 10));
 	EXPECT_EQ("test10.00test", FormatString("test%.2ftest", 10.0f));
 }
 
 TEST(FormatString, StringFormats)
-{;
+{
 	EXPECT_EQ("teststrRAWtest", FormatString("test%stest", "strRAW"));
 	EXPECT_EQ("teststrRWtest", FormatString("test%stest", std::string("strRW")));
 
